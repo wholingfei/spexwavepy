9937caac9eba1ea20d0660b0e83a70efd71144fd
Lingfei <hulf88@ustc.edu.cn>
2024-04-19 20:02:42 +0800

    autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+autodoc_mock_imports = ['numpy', 'scipy', 'matplotlib', 'natsort', 'cv2']
+
 
 # -- Options for HTML output -------------------------------------------------
 
